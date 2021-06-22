3b2d39d7bf2ff14a657411be75e0435f46657052
John <jlithio@gmail.com>
2021-06-21 21:45:26 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""This file controls how the pip-installable package is set up."""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="audl_advanced_stats",
+    version="0.0.1",
+    author="John Lithio",
+    description="For downloading and analyzing AUDL advanced stats.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JohnLithio/AUDL-Advanced-Stats",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
